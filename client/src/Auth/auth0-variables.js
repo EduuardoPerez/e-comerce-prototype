fc0ff5f239ea4654f399438a85eabf6b263ebc87
Eduardo Perez <edujpp1@gmail.com>
2020-01-03 19:14:45 -0300

    Se creo el servidor que tendrá el end-point con los productos y se está creando en el front la autenticación con Auth0 y los jason web tokens (jwt) para poder acceder solo pasando las verificaciones correspondientes

---
+++ client/src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'dev-w3hnzwlb.auth0.com',
+  clientId: '0Y4Bx6XMuuSNdJ6w634cKyZ46Bnf2LIi',
+  callbackUrl: 'http://localhost:3000/callback', 
+  apiURL: 'http://productos'
+}
--- client/src/componentes/Productos/Productos.js
+++ client/src/componentes/Productos/Productos.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import Producto from '../Producto/Producto';
-import Buscador from '../Buscador/Buscador';
-import './Productos.css';
+import React, { Component } from "react";
+import Producto from "../Producto/Producto";
+import Buscador from "../Buscador/Buscador";
+import "./Productos.css";
 
 class Productos extends Component {
 
-     render() { 
-          return ( 
-               <div className="productos">
-                    <h2>Nuestros Productos</h2>
-                    <Buscador
-                        busqueda={this.props.busquedaProducto}
-                    />
-                    <ul className="lista-productos">
-                         {Object.keys(this.props.productos).map(producto => (
-                              <Producto
-                                   informacion={this.props.productos[producto]}
-                                   key={producto}
-                              />
-                         )) }
-                    </ul>
-               </div>
-           )
-     }
+	state = {
+		productos: [],
+		terminoBusqueda: ''
+	}
+
+	componentWillMount() {
+		this.queryAPI();
+	}
+
+	queryAPI = () => {
+		console.log(this.props.auth);
+	}
+
+	login = () => {
+		this.props.auth.login();
+	}
+
+  render() {
+
+		const { isAuthenticated } = this.props.auth;
+
+    return (
+      <div className="productos">
+        <h2>Nuestros Productos</h2>
+        <Buscador
+					busqueda={this.props.busquedaProducto} 
+				/>
+        <ul className="lista-productos">
+					{/* {Object.keys(this.props.productos).map(producto => (
+						<Producto
+							informacion={this.props.productos[producto]}
+							key={producto}
+						/>
+					))} */}
+        </ul>
+				{
+					!isAuthenticated() &&  (
+						<div className="contenedor-boton">
+							<p>Para ver el contenido debes estar logueado:</p>
+							<a className="boton" onClick={ this.login }>Iniciar sesión</a>
+						</div>
+					)
+				}
+      </div>
+    );
+  }
 }
- 
+
 export default Productos;
